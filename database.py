7f1e798288e031f074c3729c8866988dc3a63762
Arman Augusto <arman.augusto@gmail.com>
2022-01-07 23:54:47 -0800

    added all functionality, ready for testing

--- database.py
+++ database.py
@@ -1,2 +1,76 @@
 # database.py
 
+import sqlite3
+from typing import List
+import datetime
+from model import ActionItem
+conn = sqlite3.connect('action_items.db')
+c = conn.cursor()
+
+
+def create_table():
+    c.execute("""CREATE TABLE IF NOT EXISTS action_items (
+            task text,
+            priority_level text,
+            date_added text,
+            date_completed text,
+            status integer,
+            position integer
+            )""")
+
+
+create_table()
+
+
+def insert_action_item(action_item: ActionItem):
+    c.execute('select count(*) FROM action_items')
+    count = c.fetchone()[0]
+    action_item.position = count if count else 0
+    with conn:
+        c.execute('INSERT INTO action_items VALUES (:task, :priority_level, :date_added, :date_completed, :status, :position)',
+        {'task': action_item.task, 'priority_level': action_item.priority_level, 'date_added': action_item.date_added,
+         'date_completed': action_item.date_completed, 'status': action_item.status, 'position': action_item.position})
+
+
+def get_all_action_items() -> List[ActionItem]:
+    c.execute('select * from action_items')
+    results = c.fetchall()
+    action_items = []
+    for result in results:
+        action_items.append(ActionItem(*result))
+    return action_items
+
+
+def delete_action_item(position):
+    c.execute('select count(*) from action_items')
+    count = c.fetchone()[0]
+
+    with conn:
+        c.execute("DELETE from action_items WHERE position=:position", {"position": position})
+        for pos in range(position+1, count):
+            change_position_(pos, pos-1, False)
+
+
+def change_position(old_position: int, new_position: int, commit=True):
+    c.execute('UPDATE action_items SET position = :position_new WHERE position = :position_old',
+                {'position_old': old_position, 'position_new': new_position})
+    if commit:
+        conn.commit()
+
+
+def update_action_item(position: int, task: str, priority_level: str):
+    with conn:
+        if task is not None and category is not None:
+            c.execute('UPDATE action_items SET task = :task, priority_level = :priority_level WHERE position = :position',
+                        {'position': position, 'task': task, 'priority_level': priority_level})
+        elif task is not None:
+            c.execute('UPDATE action_items SET task = :task WHERE position = :position',
+                        {'position': position, 'task': task})
+        elif priority_level is not None:
+            c.execute('UPDATE action_items SET priority_level = :priority_level WHERE position = :position',
+                        {'position': position, 'priority_level': priority_level})
+
+
+def complete_action_item(position: int):
+    c.execute('UPDATE action_items SET status = 2, date_completed = :date_completed WHERE position = :position',
+                {'position': position, 'date_completed': datetime.datetime.now().isoformat()})
--- priorityLevelsCLI.py
+++ priorityLevelsCLI.py
@@ -3,6 +3,8 @@
 import typer
 from rich.console import Console
 from rich.table import Table
+from model import ActionItem
+from database import get_all_action_items, delete_action_item, insert_action_item, complete_action_item, update_action_item
 
 
 console = Console()
@@ -11,17 +13,55 @@ app = typer.Typer()
 
 
 @app.command(short_help='adds an item')
-def add(task: str, category: str):
-    typer.echo(f"adding {task}, {category}")
+def add(task: str, priority_level: str):
+    typer.echo(f"adding {task}, {priority_level}")
+    action_item = ActionItem(task, priority_level)
+    insert_action_item(action_item)
+    show()
 
 @app.command()
 def delete(position: int):
     typer.echo(f"deleting {position}")
+    # indices in UI begin at 1, but in database at 0
+    delete_action_item(position-1)
+    show()
 
 app.command()
-def update(position: int, task: str = None, category: str = None):
+def update(position: int, task: str = None, priority_level: str = None):
     typer.echo(f"updating {position}")
+    update_action_item(position-1, task, priority_level)
+    show()
 
+@app.command()
+def complete(position: int):
+    typer.echo(f"complete {position}")
+    complete_action_item(position-1)
+    show()
+
+@app.command()
+def show():
+    tasks = get_all_action_items()
+    console.print("[bold magenta]\n\n\n\t\t\t******** Priority Levels CLI ********[/bold magenta]\n")
+
+    table = Table(show_header=True, header_style="bold blue")
+    table.add_column("#", style="dim", width=6)
+    table.add_column("Action Item", min_width=40)
+    table.add_column("Priority Level", min_width=20, justify="right")
+    table.add_column("Status", min_width=12, justify="right")
+
+    def get_priority_level_color(priority_level):
+        COLORS = {'High': 'red', 'Medium': 'yellow', 'Low': 'Blue'}
+        if priority_level in COLORS:
+            return COLORS[priority_level]
+        return 'white'
+    
+    for idx, task in enumerate(tasks, start=1):
+        c = get_priority_level_color(task.priority_level)
+        is_done_str = "Done" if task.status == 2 else "Incomplete"
+        table.add_row(str(idx), task.task, f'[{c}]{task.priority_level}[/{c}]', is_done_str)
+    console.print(table)
+
+    console.print("\n\n")
 
 if __name__ == "__main__":
     app()
